7eaeda5909ade26deb2745d5cd72dfd835d4d265
izniburak <izniburak@outlook.com>
2016-02-02 11:58:25 +0200

    kelime ekleme ve yüksek skor uygulaması yapıldı.

--- kelime.rb
+++ kelime.rb
@@ -9,12 +9,25 @@ class Kelime
 		@kelimeler.select { |kelime| kelime.length == sayi }
 	end
 
-
 	def liste
 		@kelimeler
 	end
 
+	def kelime_ekle (kelime)
+		raise ArgumentError unless kelime.is_a? String
+		@kelimeler << kelime
+		kaydet
+	end
+
 	private
+
+	def kaydet
+		if File.exist? "data.db" and File.readable? "data.db"
+			File.open("data.db", "w") do |dosya|
+				@kelimeler.each { |kelime| dosya.puts kelime.downcase }
+			end
+		end
+	end
 
 	def oku
 		if File.exist? "data.db" and File.readable? "data.db"
